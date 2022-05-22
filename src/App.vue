efe6a55aa9cef01fc5613da74e58e78ae8cdfdb4
FsYume <hi@fsyume.com>
2022-05-22 20:00:54 +0800

    修复与更新：无限加载bug，增加在距离底部9999px时就加载下一组数据，防止出现对不齐

--- src/App.vue
+++ src/App.vue
@@ -1,70 +1,57 @@
 <template>
-  <el-row :gutter="10">
-    <el-col :xs="24" :md="8" :lg="8">
-    <ul 
-    v-infinite-scroll="load"
-    infinite-scroll-distance="1000"
-    >
-      <div class="demo-image__lazy">
-        <el-image
-          v-loading="loading"
-          v-for="url in urls"
-          :key="url"
-          :src="url"
-          :preview-src-list="urls"
-          :initial-index="urls.indexOf(url)"
-          @load="loadingfun()"
-          lazy
-        />
-      </div>
-    </ul>
-    </el-col>
+  <div v-infinite-scroll="load" infinite-scroll-distance="9999">
+    <el-row :gutter="10">
+      <el-col :lg="8" :md="8" :xs="24">
+        <div class="demo-image__lazy">
+          <el-image
+              v-for="url in urls"
+              :key="url"
+              v-loading="loading"
+              :initial-index="urls.indexOf(url)"
+              :preview-src-list="urls"
+              :src="url"
+              lazy
+              @load="loadingfun()"
+          />
+        </div>
+      </el-col>
 
-    <el-col :xs="0" :md="8" :lg="8" :xl="8">
-    <ul 
-    v-infinite-scroll="loadA"
-    infinite-scroll-distance="1000"
-    >
-      <div class="demo-image__lazy">
-        <el-image
-          v-loading="loading"
-          v-for="url in urlsA"
-          :key="url"
-          :src="url"
-          :preview-src-list="urlsA"
-          :initial-index="urlsA.indexOf(url)"
-          @load="loadingfun"
-          lazy
-        />
-      </div>
-      </ul>
-    </el-col>
+      <el-col :lg="8" :md="8" :xl="8" :xs="0">
+        <div class="demo-image__lazy">
+          <el-image
+              v-for="url in urlsA"
+              :key="url"
+              v-loading="loading"
+              :initial-index="urlsA.indexOf(url)"
+              :preview-src-list="urlsA"
+              :src="url"
+              lazy
+              @load="loadingfun"
+          />
+        </div>
+      </el-col>
 
-    <el-col :xs="0" :md="8" :lg="8" :xl="8">
-    <ul 
-    v-infinite-scroll="loadB"
-    infinite-scroll-distance="1000"
-    >
-      <div class="demo-image__lazy">
-        <el-image
-          v-loading="loading"
-          v-for="url in urlsB"
-          :key="url"
-          :src="url"
-          :preview-src-list="urlsB"
-          :initial-index="urlsB.indexOf(url)"
-          @load="loadingfun"
-          lazy
-        />
-      </div>
-      </ul>
-    </el-col>
-  </el-row>
+      <el-col :lg="8" :md="8" :xl="8" :xs="0">
+        <div class="demo-image__lazy">
+          <el-image
+              v-for="url in urlsB"
+              :key="url"
+              v-loading="loading"
+              :initial-index="urlsB.indexOf(url)"
+              :preview-src-list="urlsB"
+              :src="url"
+              lazy
+              @load="loadingfun"
+          />
+        </div>
+      </el-col>
+    </el-row>
+  </div>
 </template>
 
 <script lang="ts" setup>
-import { onMounted, ref } from "vue";
-import { ElNotification } from "element-plus";
+import {onMounted, ref} from "vue";
+import {ElNotification} from "element-plus";
 import axios from "axios"
 
 // 生命周期函数
@@ -84,72 +71,45 @@ var urlsA = ref([])
 var urlsB = ref([])
 
 
-function loadingfun(){
+function loadingfun() {
   loading.value = false
 }
 
 // 底部加载
-function load(){
+function load() {
   addpic(urls)
+  addpic(urlsA)
+  addpic(urlsB)
 }
 
 function loadA() {
-    addpic(urlsA)
+  addpic(urlsA)
 }
 
 function loadB() {
-    addpic(urlsB)
+  addpic(urlsB)
 }
+
 // 非R18数组
-function piclist(urllist){
+function piclist(urllist) {
   axios.get("https://sakura.iw233.cn/API/list/acg/json.php").then(
-    (res)=>{
-      urllist.value = res.data.data
-    }
+      (res) => {
+        urllist.value = res.data.data
+      }
   )
 }
 
 // 底部加载增加图片
-function addpic(urllist){
+function addpic(urllist) {
   axios.get("https://sakura.iw233.cn/API/list/acg/json.php").then(
-    (res)=>{
-      urllist.value.push(res.data.data[1])
-    }
+      (res) => {
+        let list = res.data.data
+        for (let i in list) {
+          console.log("for in 循环" + list[i])
+          urllist.value.push(list[i])
+        }
+      }
   )
-}
-
-// 涩图链接数组生成（R18）
-function fortxt() {
-  // 总涩图数
-  var num = 262
-
-  var t1 = "https://i.hifsyu.me/pixiv-webp/pixiv_r18_";
-  var t2 = ".webp";
-
-  var urls = [];
-
-  for (var i = 1; i <= num; i++) {
-    var t3 = t1 + String(i) + t2;
-
-    urls.push(t3);
-  }
-
-  return urls;
-}
-
-// 洗牌卡算法
-function shuffle(array) {
-  let copy = [];
-
-  let len = array.length;
-
-  while (len) {
-    //随机生成 0  到  n-1 的数字 , 作为数组下标
-    let index = Math.floor(Math.random() * len--);
-    //把随机到的数字放入新数组中,并从原数组中删除
-    copy.push(array.splice(index, 1)[0]);
-  }
-  return copy.slice(0,1);
 }
 
 const open = () => {
